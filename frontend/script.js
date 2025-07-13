0fb61bee2bb1c53e5fad901c1b6028e4442859ef
GriffinBuschjost <glbcvd@umsystem.edu>
2025-07-13 17:49:33 -0500

    added autofill for names

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,77 @@
 const video = document.getElementById("swing");
 const feedback = document.getElementById("feedback");
+const datalist = document.getElementById("golfers");
 
 const clips = [
   { file: "Poston_swing_web_ready.mp4", answer: "J.T. Poston" },
   { file: "Bryson_swing_web_ready.mp4", answer: "Bryson DeChambeau" },
   // Add more here
 ];
+
+const golferNames = [
+  "Scottie Scheffler",
+  "Rory McIlroy",
+  "Xander Schauffele",
+  "Justin Thomas",
+  "Collin Morikawa",
+  "Russell Henley",
+  "Keegan Bradley",
+  "J.J. Spaun",
+  "Ludvig Åberg",
+  "Sepp Straka",
+  "Hideki Matsuyama",
+  "Viktor Hovland",
+  "Tommy Fleetwood",
+  "Robert MacIntyre",
+  "Bryson DeChambeau",
+  "Maverick McNealy",
+  "Ben Griffin",
+  "Shane Lowry",
+  "Harris English",
+  "Patrick Cantlay",
+  "Sam Burns",
+  "Tyrrell Hatton",
+  "Justin Rose",
+  "Corey Conners",
+  "Sungjae Im",
+  "Brian Harman",
+  "Billy Horschel",
+  "Jason Day",
+  "Ryan Fox",
+  "Nick Taylor",
+  "Daniel Berger",
+  "Wyndham Clark",
+  "Aaron Rai",
+  "Akshay Bhatia",
+  "Min Woo Lee",
+  "Thomas Detry",
+  "Taylor Pendrith",
+  "Cameron Young",
+  "Denny McCarthy",
+  "Adam Scott",
+  "Lucas Glover",
+  "J.T. Poston",
+  "Tony Finau",
+  "Sahith Theegala",
+  "Byeong Hun An",
+  "Jordan Spieth",
+  "Tom Hoge",
+  "Will Zalatoris",
+  "Gary Woodland",
+  "Max Homa",
+  "Davis Riley",
+  "Mito Pereira",
+  "Chris Kirk",
+  "Matt Fitzpatrick"
+];
+
+// 🧠 Populate datalist options
+golferNames.forEach(name => {
+  const option = document.createElement("option");
+  option.value = name;
+  datalist.appendChild(option);
+});
+
 
 let current = 0;
 video.src = "silhouettes/" + clips[current].file;
